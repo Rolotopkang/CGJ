b4c86648778ec7eec907d1378f7d81e3f2dfd91e
Lviat Yi <LviatYi@qq.com>
2022-01-23 21:46:15 +0800

    Update Click Start

--- Assets/scrips/StartClick.cs
+++ Assets/scrips/StartClick.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainUIManager : MonoBehaviour {
+public class StartClick : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-
+        this.GetComponent<Button>().onClick.AddListener(clicked);
     }
 
     // Update is called once per frame
     void Update() {
 
     }
-
-    public void OnClick() {
+    void clicked() {
         SceneManager.LoadScene("MainScene");
     }
 }
